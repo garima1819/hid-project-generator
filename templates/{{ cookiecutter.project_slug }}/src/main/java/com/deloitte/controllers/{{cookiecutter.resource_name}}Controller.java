70ddf58a43ee82e2edaa738cc767f56f05a1c9d8
Krishnkr <visitkrishna92@gmail.com>
2023-01-31 23:19:28 +0530

    Beautified the controller

--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controllers/{{cookiecutter.resource_name}}Controller.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controllers/{{cookiecutter.resource_name}}Controller.java
@@ -3,6 +3,7 @@ package com.deloitte.controllers;
 import com.deloitte.domain.{{cookiecutter.resource_name}};
 import com.deloitte.service.{{cookiecutter.resource_name}}Service;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,27 +14,27 @@ public class {{cookiecutter.resource_name}}Controller {
     @Autowired
     private {{cookiecutter.resource_name}}Service {{cookiecutter.resource_name|lower}}Service;
 
-    @GetMapping("/{{cookiecutter.resource_name}}s")
+    @GetMapping("/{{cookiecutter.resource_name|lower}}s")
     public List<{{cookiecutter.resource_name}}> get{{cookiecutter.resource_name}}s() {
         return {{cookiecutter.resource_name|lower}}Service.get{{cookiecutter.resource_name}}s();
     }
 
-    @GetMapping("/{{cookiecutter.resource_name}}s/{id}")
+    @GetMapping("/{{cookiecutter.resource_name|lower}}/{id}")
     public {{cookiecutter.resource_name}} get{{cookiecutter.resource_name}}ById(@PathVariable Long id) {
         return {{cookiecutter.resource_name|lower}}Service.get{{cookiecutter.resource_name}}ById(id);
     }
 
-    @PostMapping("/{{cookiecutter.resource_name}}s")
+    @PostMapping("/{{cookiecutter.resource_name|lower}}")
     public {{cookiecutter.resource_name}} save{{cookiecutter.resource_name}}(@RequestBody {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
         return {{cookiecutter.resource_name|lower}}Service.save{{cookiecutter.resource_name}}({{cookiecutter.resource_name|lower}});
     }
 
-    @PutMapping("/{{cookiecutter.resource_name}}s/{id}")
+    @PutMapping("/{{cookiecutter.resource_name|lower}}/{id}")
     public {{cookiecutter.resource_name}} update{{cookiecutter.resource_name}}(@PathVariable Long id, @RequestBody {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
         return {{cookiecutter.resource_name|lower}}Service.update{{cookiecutter.resource_name}}(id, {{cookiecutter.resource_name|lower}});
     }
 
-    @DeleteMapping("/{{cookiecutter.resource_name}}s/{id}")
+    @DeleteMapping("/{{cookiecutter.resource_name|lower}}/{id}")
     public void delete{{cookiecutter.resource_name}}(@PathVariable Long id) {
         {{cookiecutter.resource_name|lower}}Service.delete{{cookiecutter.resource_name}}ById(id);
     }
