b914e428112716009194fe0589234630b302e0f1
Krishnkr <visitkrishna92@gmail.com>
2023-01-31 17:10:34 +0530

    Added required imports

--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controllers/{{cookiecutter.resource_name}}Controller.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controllers/{{cookiecutter.resource_name}}Controller.java
@@ -1,5 +1,7 @@
 package com.deloitte.controllers;
 
+import com.deloitte.domain.{{cookiecutter.resource_name}};
+import com.deloitte.domain.{{cookiecutter.resource_name}}Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/{{cookiecutter.project_slug}}Application.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/{{cookiecutter.project_slug}}Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class {{cookiecutter.resource_name}}Application {
+public class {{cookiecutter.project_slug}}Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run({{cookiecutter.resource_name}}Application.class, args);
