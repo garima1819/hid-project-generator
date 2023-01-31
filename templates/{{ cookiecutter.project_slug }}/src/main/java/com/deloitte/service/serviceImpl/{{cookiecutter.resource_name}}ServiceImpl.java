6a1fc6dd8f8b60a86356baccfb9e8124a87e2aac
Aggarwal, Garima <gariaggarwal@deloitte.com>
2023-01-31 13:44:57 +0530

    Modifications in service files

--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
@@ -1,9 +1,10 @@
 package com.deloitte.service.serviceImpl;
 
+import com.deloitte.service.{{cookiecutter.resource_name}}Service;
 import java.util.Collection;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-//import com.deloitte.Product; to be confirmed
 
 @Service
 public class {{cookiecutter.resource_name}}ServiceImpl implements {{cookiecutter.resource_name}}Service {
@@ -12,24 +13,29 @@ public class {{cookiecutter.resource_name}}ServiceImpl implements {{cookiecutter
     {{cookiecutter.resource_name}}Repository {{cookiecutter.resource_name|lower}}Repository;
 
     @Override
-    public {{cookiecutter.resource_name}} create{{cookiecutter.resource_name}}({{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
+    public {{cookiecutter.resource_name}} save{{cookiecutter.resource_name}}({{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
         return {{cookiecutter.resource_name}}Repository.save({{cookiecutter.resource_name|lower}});
     }
 
     @Override
-    public {{cookiecutter.resource_name}} update{{cookiecutter.resource_name}}(Object id,{{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
-        {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}Obj={{cookiecutter.resource_name|lower}}Repository.get(id);
+    public {{cookiecutter.resource_name}} update{{cookiecutter.resource_name}}(Long id,{{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}) {
+        {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}}Obj={{cookiecutter.resource_name|lower}}Repository.findById(id).get();
         //update the required fields
         return {{cookiecutter.resource_name|lower}}Repository.save({{cookiecutter.resource_name|lower}}Obj);
     }
 
     @Override
-    public void delete{{cookiecutter.resource_name}}(Object id) {
+    public void delete{{cookiecutter.resource_name}}ById(Long id) {
         {{cookiecutter.resource_name|lower}}Repository.deleteById(id);
     }
 
     @Override
     public Collection<{{cookiecutter.resource_name}}>get{{cookiecutter.resource_name}}s() {
         return {{cookiecutter.resource_name|lower}}Repository.findAll();
+    }
+
+    @Override
+    public {{cookiecutter.resource_name}} get{{cookiecutter.resource_name}}ById(Long id) {
+        return {{cookiecutter.resource_name|lower}}Repository.findById(id).get();
     }
 }
--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/service/{{cookiecutter.resource_name}}Service.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/service/{{cookiecutter.resource_name}}Service.java
@@ -1,13 +1,13 @@
 package com.deloitte.service;
 
-import java.util.Collection;
-//import com.deloitte.Product; to be confirmed
+import java.util.List;
 
 public interface {{cookiecutter.resource_name}}Service {
 
-    public void create{{cookiecutter.resource_name}}({{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}});
-    public void update{{cookiecutter.resource_name}}(Object id, {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}});
-    public void delete{{cookiecutter.resource_name}}(Object id);
-    public Collection<{{cookiecutter.resource_name}}> get{{cookiecutter.resource_name}}s();
+    public void save{{cookiecutter.resource_name}}({{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}});
+    public void update{{cookiecutter.resource_name}}(Long id, {{cookiecutter.resource_name}} {{cookiecutter.resource_name|lower}});
+    public void delete{{cookiecutter.resource_name}}ById(Long id);
+    public List<{{cookiecutter.resource_name}}> get{{cookiecutter.resource_name}}s();
+    public {{cookiecutter.resource_name}} get{{cookiecutter.resource_name}}ById(Long id);
 
 }
