60253f1567a4b373d88560d815984ce800b1e688
Krishnkr <visitkrishna92@gmail.com>
2023-02-01 18:33:19 +0530

    Added @Sl4j for logging

--- templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controller/{{cookiecutter.resource_name}}Controller.java
+++ templates/{{ cookiecutter.project_slug }}/src/main/java/com/deloitte/controller/{{cookiecutter.resource_name}}Controller.java
@@ -2,18 +2,17 @@ package com.deloitte.controller;
 
 import com.deloitte.domain.{{cookiecutter.resource_name}};
 import com.deloitte.service.{{cookiecutter.resource_name}}Service;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+
+@Slf4j
 @RestController
 public class {{cookiecutter.resource_name}}Controller {
 
-    private static final Logger log = LoggerFactory.getLogger({{cookiecutter.resource_name}}Controller.class);
 
     @Autowired
     private {{cookiecutter.resource_name}}Service {{cookiecutter.resource_name|lower}}Service;
