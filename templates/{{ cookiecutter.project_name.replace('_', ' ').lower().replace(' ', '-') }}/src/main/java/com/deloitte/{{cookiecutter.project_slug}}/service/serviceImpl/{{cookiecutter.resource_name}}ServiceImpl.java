d628408c376e04c0119fa136b74f013aafe683ff
Krishnkr <visitkrishna92@gmail.com>
2023-02-01 20:50:54 +0530

    Change project structure

--- templates/{{ cookiecutter.project_name.replace('_', ' ').lower().replace(' ', '-') }}/src/main/java/com/deloitte/{{cookiecutter.project_slug}}/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
+++ templates/{{ cookiecutter.project_name.replace('_', ' ').lower().replace(' ', '-') }}/src/main/java/com/deloitte/{{cookiecutter.project_slug}}/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
@@ -1,8 +1,8 @@
-package com.deloitte.service.serviceImpl;
+package com.deloitte.{{cookiecutter.project_slug}}.service.serviceImpl;
 
-import com.deloitte.domain.{{cookiecutter.resource_name}};
-import com.deloitte.repository.{{cookiecutter.resource_name}}Repository;
-import com.deloitte.service.{{cookiecutter.resource_name}}Service;
+import com.deloitte.{{cookiecutter.project_slug}}.domain.{{cookiecutter.resource_name}};
+import com.deloitte.{{cookiecutter.project_slug}}.repository.{{cookiecutter.resource_name}}Repository;
+import com.deloitte.{{cookiecutter.project_slug}}.service.{{cookiecutter.resource_name}}Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ templates/{{ cookiecutter.project_name.replace('_', ' ').lower().replace(' ', '-') }}/src/main/java/com/deloitte/{{cookiecutter.project_slug}}/{{ cookiecutter.project_name.replace('_', ' ').replace('-', ' ').title().replace(' ', '') }}Application.java
@@ -0,0 +1,20 @@
+package com.deloitte.{{cookiecutter.project_slug}};
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class {{ cookiecutter.project_name.replace('_', ' ').replace('-', ' ').title().replace(' ', '') }}Application {
+
+
+	public static void main(String[] args) {
+
+		log.info("Application Starting...");
+		SpringApplication.run({{ cookiecutter.project_name.replace('_', ' ').replace('-', ' ').title().replace(' ', '') }}Application.class, args);
+		log.info("Application Started");
+
+	}
+
+}
