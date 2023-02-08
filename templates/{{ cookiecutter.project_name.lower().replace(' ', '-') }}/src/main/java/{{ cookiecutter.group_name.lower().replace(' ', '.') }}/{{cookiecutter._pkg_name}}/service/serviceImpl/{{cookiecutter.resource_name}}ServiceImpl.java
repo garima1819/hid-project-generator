55c145d6894ea6d0058d71095091e14597d03683
garima1819 <122097806+garima1819@users.noreply.github.com>
2023-02-08 17:17:09 +0530

    removed unused imports

--- templates/{{ cookiecutter.project_name.lower().replace(' ', '-') }}/src/main/java/{{ cookiecutter.group_name.lower().replace(' ', '.') }}/{{cookiecutter._pkg_name}}/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
+++ templates/{{ cookiecutter.project_name.lower().replace(' ', '-') }}/src/main/java/{{ cookiecutter.group_name.lower().replace(' ', '.') }}/{{cookiecutter._pkg_name}}/service/serviceImpl/{{cookiecutter.resource_name}}ServiceImpl.java
@@ -7,12 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import com.deloitte.{{cookiecutter._pkg_name}}.exception.ResourceNotFoundException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @Slf4j
 @Service
